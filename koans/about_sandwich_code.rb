35fd2f9e88df10739aa14efc58bfe2dbd5ee0f70
Julian Braasch <julian.braasch@me.com>
2017-11-18 10:40:11 +0100

    184

--- koans/about_sandwich_code.rb
+++ koans/about_sandwich_code.rb
@@ -80,11 +80,17 @@ class AboutSandwichCode < Neo::Koan
   # ------------------------------------------------------------------
 
   def find_line2(file_name)
-    # Rewrite find_line using the file_sandwich library function.
+    file_sandwich(file_name) do |file|
+      count = 0
+      while file.gets
+        count += 1
+      end
+      count
+    end
   end
 
   def test_finding_lines2
-    assert_equal __, find_line2("example_file.txt")
+    assert_equal 4, find_line2("example_file.txt")
   end
 
   # ------------------------------------------------------------------
@@ -100,7 +106,7 @@ class AboutSandwichCode < Neo::Koan
   end
 
   def test_open_handles_the_file_sandwich_when_given_a_block
-    assert_equal __, count_lines3("example_file.txt")
+    assert_equal 4, count_lines3("example_file.txt")
   end
 
 end

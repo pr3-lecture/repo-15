62a33dc8941dbcb9943240abe35a87e05412eba9
Julian Braasch <Julian@Julians-MBP.fritz.box>
2018-01-08 18:59:37 +0100

    Umständlichen Regex durch strstr() ersetzt

--- crypter/Main.c
+++ crypter/Main.c
@@ -1,39 +1,8 @@
 #include <memory.h>
 #include <stdio.h>
 #include "crypto.h"
-#include "regex.h"
 
 //#define DEBUG
-
-/**
- * Extrahiert aus dem Aufrufpfad den Modus
- * @param aufrufpfad Aufrufpfad
- * @return 0 (ENCRYPT) / 1 (DECRYPT)
- */
-int getModus(char* aufrufpfad) {
-    regex_t regex;
-    int res;
-    int ergebnis = -1;
-
-    /* Encrypt */
-    regcomp(&regex, "encrypt", 0);
-    res = regexec(&regex, aufrufpfad, 0, NULL, 0);
-
-    if (!res) {
-        ergebnis = ENCRYPT;
-    }
-
-    /* Decrypt */
-    regcomp(&regex, "decrypt", 0);
-    res = regexec(&regex, aufrufpfad, 0, NULL, 0);
-
-    if (!res) {
-        ergebnis = DECRYPT;
-    }
-
-    regfree(&regex);
-    return ergebnis;
-}
 
 int main(int args, char** argv) {
     KEY k;
@@ -66,14 +35,14 @@ int main(int args, char** argv) {
     printf("Modus: %s\n", argv[0]);
 #endif
 
-    /* Modus bestimmen */
-    int modus = getModus(argv[0]);
-
-    if (modus == ENCRYPT) {
+    /* Encrypt */
+    if (strstr(argv[0], "encrypt")) {
         if (encrypt(k, eingabe, ergebnis) == 0) {
             printf("Nachricht: %s\nKey: %s\nErgebnis: %s\n", eingabe, argv[1], ergebnis);
         }
-    } else if (modus == DECRYPT) {
+    }
+    /* Decrypt */
+    else if (strstr(argv[0], "decrypt")) {
         if (decrypt(k, eingabe, ergebnis) == 0) {
             printf("Nachricht: %s\nKey: %s\nErgebnis: %s\n", eingabe, argv[1], ergebnis);
         }
